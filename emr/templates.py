ebbcacd2b366615d069b7ce6a5cdee15f7aa0cfc
Caleb Sabatini <calebsabatini@gmail.com>
2017-11-17 10:35:41 -0600

    mocked api tests for streaming shutdown flag

--- emr/templates.py
+++ emr/templates.py
@@ -1,5 +1,19 @@
 from jinja2 import Template
 
+emr_create_cluster_template = Template('''aws emr create-cluster{% if not airflow %} --profile {{ profile }}{% endif %}
+    \t--name {{ cluster_name }}
+    \t--release-label {{ emr_version }}
+    \t--configurations file://configurations.json
+    \t--{{ term_choice }}
+    \t--service-role {{ emr_service_role }}
+    \t--tags env={{ env }} owner=bigdata project={{ project }} Name={{ cluster_name }}
+    \t--applications Name=Hadoop Name=Spark Name=Hue 
+    \t--bootstrap-actions Path=s3://{{ env }}-sonic-artifacts/scripts/download_service.sh,Name=S3DownloadService,Args=[{% if artifact_path %}{{ job_runtime }},{{ artifact_path }}{% endif %}]
+    \t--ec2-attributes InstanceProfile={{ emr_instance_profile }},KeyName={{ emr_ssh_key_name }},SubnetIds=[\'{{ master_private_subnet_app_secondary_id }}\'],ServiceAccessSecurityGroup={{ master_emrservice_security_group_id }},EmrManagedMasterSecurityGroup={{ master_emr_security_group_id }},EmrManagedSlaveSecurityGroup={{ master_emr_security_group_id }} 
+    \t--instance-groups InstanceGroupType=MASTER,InstanceCount=1,InstanceType={{ master_type }} InstanceGroupType=CORE,InstanceType={{ core_type }},InstanceCount={{ core_count }} 
+    \t{% if artifact_path %}--steps{% if false %} Type=CUSTOM_JAR,Jar=command-runner.jar,Name=S3DistCp,ActionOnFailure=CONTINUE,Args=[s3-dist-cp,--s3Endpoint=s3.amazonaws.com,--src=s3://{{ checkpoint_bucket }}/{{ job_name }}/,--dest=/checkpoints/,--srcPattern=".*"]{% endif %} Type=Spark,Name={{ job_name }},ActionOnFailure=CONTINUE,Args={{ step_args }}{% endif %}
+''')
+
 # for pyspark we run spark spark-submit with basically just --py-files, for JVM we have more configurations
 spark_template = Template('''--deploy-mode cluster --master yarn 
 {% if job_configs %}{{ job_configs }} {% endif %}
@@ -18,11 +32,11 @@ spark_template = Template('''--deploy-mode cluster --master yarn
 ''')
 
 add_spark_step_template = \
-    Template('aws emr add-steps{% if not airflow %} --profile {{ profile }}{% endif %} --cluster-id {{ cluster_id }}'
+    Template('aws emr add-steps{% if not airflow %} --profile {{ profile }}{% endif %} --cluster-id {{ cluster_id }} '
              '--steps Type=Spark,Name={{ job_name }},ActionOnFailure=CONTINUE,Args={{ step_args }}')
 
 add_checkpoint_cp_step_template = \
-    Template('aws emr add-steps{% if not airflow %} --profile {{ profile }}{% endif %} --cluster-id {{ cluster_id }}'
+    Template('aws emr add-steps{% if not airflow %} --profile {{ profile }}{% endif %} --cluster-id {{ cluster_id }} '
              '--steps Type=CUSTOM_JAR,Jar=command-runner.jar,Name=S3DistCp,ActionOnFailure=CONTINUE,'
              'Args=[s3-dist-cp,--s3Endpoint=s3.amazonaws.com,--src=/checkpoints/,'
              '--dest=s3://{{ checkpoint_bucket }}/{{ job_name }}/,--srcPattern=".*"]')
--- test/test_job_client.py
+++ test/test_job_client.py
@@ -1,5 +1,6 @@
-from mock import Mock
+from mock import call, Mock
 from emr.job_client import handle_job_request
+import copy
 
 params = {
     "airflow": False,
@@ -24,7 +25,8 @@ params = {
 }
 
 mock_api = Mock()
-mock_api.get_emr_cluster_with_name.return_value = [{'id': 'abc123'}]
+mock_api.get_emr_cluster_with_name.return_value = [{'id': 'cluster123'}]
+mock_api.list_cluster_steps.return_value = ['step123']
 mock_api.list_running_cluster_instances.return_value = {
     'Instances': [
         {'PrivateIpAddress': '192.168.0.1'},
@@ -37,5 +39,28 @@ mock_api.list_running_cluster_instances.return_value = {
 def test_succeeds_with_default_params():
     output = handle_job_request(params, mock_api)
     assert len(output) > 0
-    assert output == "aws emr add-steps --profile qa --cluster-id abc123 --steps Type=Spark,Name=BTYD,ActionOnFailure=CONTINUE,Args=[--deploy-mode,cluster,--master,yarn,--conf,'spark.app.name=BTYD',--conf,'spark.yarn.max.executor.failures=8',--conf,'spark.yarn.appMasterEnv.ENVIRONMENT=qa',--py-files,s3://qa-sonic-enterprise-data-hub/artifacts/btyd-1.0.0/application.zip,s3://qa-sonic-enterprise-data-hub/artifacts/btyd-1.0.0/main.py]"
+    assert output == "aws emr add-steps --profile qa --cluster-id cluster123 --steps Type=Spark,Name=BTYD,ActionOnFailure=CONTINUE,Args=[--deploy-mode,cluster,--master,yarn,--conf,'spark.app.name=BTYD',--conf,'spark.yarn.max.executor.failures=8',--conf,'spark.yarn.appMasterEnv.ENVIRONMENT=qa',--py-files,s3://qa-sonic-enterprise-data-hub/artifacts/btyd-1.0.0/application.zip,s3://qa-sonic-enterprise-data-hub/artifacts/btyd-1.0.0/main.py]"
 
+
+def test_shutdown_false_no_shutdown_api_calls():
+    handle_job_request(params, mock_api) # shutdown=False
+
+    # api.put_job_shutdown_marker not called
+    assert mock_api.put_job_shutdown_marker.call_count == 0
+
+    # api.add_checkpoint_copy_job not called
+    assert mock_api.add_checkpoint_copy_job.call_count == 0
+
+def test_shutdown_true_makes_shutdown_api_calls():
+    test_params = copy.deepcopy(params)
+    test_params['shutdown'] = True
+
+    handle_job_request(test_params, mock_api)
+
+    # api.put_job_shutdown_marker is called for this job
+    calls = [call('qa-checkpoints', 'BTYD')]
+    assert mock_api.put_job_shutdown_marker.call_count == 1
+    mock_api.put_job_shutdown_marker.assert_has_calls(calls)
+
+    # api.add_checkpoint_copy_job is called
+    assert mock_api.add_checkpoint_copy_job.call_count == 1
