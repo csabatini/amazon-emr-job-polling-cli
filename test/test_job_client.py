fa506b2db5e9fc5ab829e6e24938271c12ef3911
Caleb Sabatini <calebsabatini@gmail.com>
2018-01-06 14:54:50 -0600

    Mock responses for grequests

--- test/test_job_client.py
+++ test/test_job_client.py
@@ -1,28 +1,29 @@
-from mock import Mock
-from emr.job_client import handle_job_request
 import copy
+import textwrap
 import pytest
+from collections import namedtuple
+from mock import Mock, call
+
+import emr.job_client
 
 params = {
     "airflow": False,
     "artifact_path": "s3://us-east-1.elasticmapreduce/samples/wordcount/",
     "checkpoint_bucket": "",
-    "cicd": False,
     "cluster_name": "Sandbox",
-    "job_name": "WordCount",
-    "job_runtime": "Scala",
-    "job_timeout": 60,
-    "job_mode": "batch",
-    "poll_cluster": False,
-    "profile": "qa",
-    "shutdown": False,
     "dryrun": True,
     "emr_version": "emr-5.6.0",
     "env": "qa",
     "job_args": "",
+    "job_name": "WordCount",
+    "job_runtime": "Python",
+    "job_timeout": 60,
+    "job_mode": "batch",
     "main_class": "org.apache.hadoop.tools.WordCount",
-    "profile": "cicd-qa",
+    "poll_cluster": False,
+    "profile": "qa",
     "project": "",
+    "shutdown": False,
     "terminate": True
 }
 
@@ -30,73 +31,94 @@ params = {
 @pytest.fixture
 def aws_api():
     mock_api = Mock()
-    mock_api.get_emr_cluster_with_name.return_value = [{'id': 'cluster123'}]
+    mock_api.get_emr_cluster_with_name.return_value = [{'id': 'cluster-123'}]
     mock_api.is_cluster_active.return_value = False
-    mock_api.list_cluster_steps.return_value = ['step123']
-    mock_api.get_remote_state_values.return_value = {
-        'master_private_subnet_app_secondary_id': 'subnet-3c2f6611',
-        'master_emrservice_security_group_id': 'sg-85e456f4',
-        'master_emr_security_group_id': 'sg-48209339'
+    mock_api.list_cluster_steps.return_value = ['step-123']
+    mock_api.list_running_cluster_instances.return_value = {
+        'Instances': [
+            {'PrivateIpAddress': '192.168.0.1'},
+            {'PrivateIpAddress': '192.168.0.2'}
+        ]
     }
     return mock_api
 
 
-def test_succeeds_with_default_params(aws_api):
-    output = handle_job_request(params, aws_api)
-    assert len(output) > 0
-    assert output == "aws emr add-steps --profile cicd-qa --cluster-id cluster123 --steps Type=Spark,Name=WordCount,ActionOnFailure=CONTINUE,Args=[--deploy-mode,cluster,--master,yarn,--conf,'spark.app.name=BTYD',--conf,'spark.yarn.max.executor.failures=8',--conf,'spark.yarn.appMasterEnv.ENVIRONMENT=qa',--py-files,s3://qa-sonic-enterprise-data-hub/artifacts/btyd-1.0.0/application.zip"
+@pytest.fixture
+def grequests(mocker):
+    Response = namedtuple('Response', ['status_code'])
+    responses = [Response(200), Response(200)]
+
+    mock_grequests = mocker.patch('emr.job_client.grequests', autospec=True)
+    mock_grequests.post.return_value = None
+    mock_grequests.get.return_value = None
+    mock_grequests.map.return_value = responses
+    return mock_grequests
 
 
-# def test_shutdown_false_no_shutdown_api_calls():
-    # mock = get_mock_api()
-    # handle_job_request(params, mock)  # shutdown=False
+def test_adds_expected_python_job(aws_api, grequests):
+    expected_output = \
+        """
+        aws emr add-steps --profile qa --cluster-id cluster-123
+         --steps Type=Spark,Name=WordCount,ActionOnFailure=CONTINUE,
+        Args=[--deploy-mode,cluster,--master,yarn,--conf,
+        'spark.app.name=WordCount',
+        --conf,'spark.yarn.appMasterEnv.ENVIRONMENT=qa',--py-files,
+        s3://us-east-1.elasticmapreduce/samples/wordcount/application.zip,
+        s3://us-east-1.elasticmapreduce/samples/wordcount/main.py]
+        """
 
-    # # api.put_job_shutdown_marker not called
-    # assert mock.put_job_shutdown_marker.call_count == 0
-
-    # # api.add_checkpoint_copy_job not called
-    # assert mock.add_checkpoint_copy_job.call_count == 0
-
-
-# def test_shutdown_true_makes_shutdown_api_calls():
-    # mock = get_mock_api()
-    # test_params = copy.deepcopy(params)
-    # test_params['shutdown'] = True
-
-    # handle_job_request(test_params, mock)
-
-    # # api.put_job_shutdown_marker is called for this job
-    # calls = [call('qa-checkpoints', 'BTYD')]
-    # assert mock.put_job_shutdown_marker.call_count == 1
-    # mock.put_job_shutdown_marker.assert_has_calls(calls)
-
-    # # api.add_checkpoint_copy_job is called
-    # assert mock.add_checkpoint_copy_job.call_count == 1
+    output = emr.job_client.handle_job_request(params, aws_api)
+    assert output
+    assert textwrap.dedent(expected_output).replace('\n', '') == output
 
 
-# def test_cluster_job_not_added_conditions():
-    # mock = get_mock_api()
-    # test_params = copy.deepcopy(params)
+def test_shutdown_false_no_shutdown_api_calls(aws_api, grequests):
+    test_params = copy.deepcopy(params)
+    test_params['job_runtime'] = 'Scala'
+    emr.job_client.handle_job_request(test_params, aws_api)
 
-    # # job should not be added if either artifact_path is empty, or shutdown is True
-    # test_params['artifact_path'] = 's3://qa-sonic-enterprise-data-hub/artifacts/btyd-1.0.0/'
-    # test_params['shutdown'] = True
-    # handle_job_request(test_params, mock)
+    # api.put_job_shutdown_marker not called
+    assert aws_api.put_job_shutdown_marker.call_count == 0
 
-    # # this API call_count will be 0 unless the job is added
-    # assert mock.list_running_cluster_instances.call_count == 0
-
-    # test_params['artifact_path'] = ''
-    # test_params['shutdown'] = False
-    # handle_job_request(test_params, mock)
-
-    # assert mock.list_running_cluster_instances.call_count == 0
-
-    # # job should be added under these parameters
-    # test_params['artifact_path'] = 's3://qa-sonic-enterprise-data-hub/artifacts/btyd-1.0.0/'
-    # test_params['shutdown'] = False
-    # handle_job_request(test_params, mock)
-
-    # assert mock.list_running_cluster_instances.call_count == 1
+    # api.add_checkpoint_copy_job not called
+    assert aws_api.add_checkpoint_copy_job.call_count == 0
 
 
+def test_shutdown_true_makes_shutdown_api_calls(aws_api, grequests):
+    test_params = copy.deepcopy(params)
+    test_params['job_runtime'] = 'Scala'
+    test_params['shutdown'] = True
+
+    emr.job_client.handle_job_request(test_params, aws_api)
+
+    # api.put_job_shutdown_marker is called for this job
+    calls = [call('qa-checkpoints', 'WordCount')]
+    assert aws_api.put_job_shutdown_marker.call_count == 1
+    aws_api.put_job_shutdown_marker.assert_has_calls(calls)
+
+    # api.add_checkpoint_copy_job is called
+    assert aws_api.add_checkpoint_copy_job.call_count == 1
+
+
+def test_cluster_job_not_added_conditions(aws_api, grequests):
+    test_params = copy.deepcopy(params)
+
+    # job should not be added if artifact_path is empty, or shutdown is True
+    test_params['shutdown'] = True
+    emr.job_client.handle_job_request(test_params, aws_api)
+    # TODO: assert on mock of run_cli_cmd
+    assert aws_api.list_running_cluster_instances.call_count == 0
+
+    test_params['artifact_path'] = ''
+    test_params['shutdown'] = False
+    emr.job_client.handle_job_request(test_params, aws_api)
+    # TODO: assert on mock of run_cli_cmd
+    assert aws_api.list_running_cluster_instances.call_count == 0
+
+    # job should be added under these parameters
+    test_params['artifact_path'] = \
+        's3://us-east-1.elasticmapreduce/samples/wordcount/'
+    test_params['shutdown'] = False
+    emr.job_client.handle_job_request(test_params, aws_api)
+    # TODO: assert on mock of run_cli_cmd
+    assert aws_api.list_running_cluster_instances.call_count == 1
