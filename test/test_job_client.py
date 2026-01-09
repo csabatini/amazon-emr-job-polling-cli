19eb801fcc849f31be1dea0d0979a9053ea2cc5a
Caleb Sabatini <calebsabatini@gmail.com>
2026-01-09 13:24:09 -0600

    Removed features specific to Spark Streaming

--- test/test_job_client.py
+++ test/test_job_client.py
@@ -2,7 +2,6 @@ import copy
 import textwrap
 import pytest
 import pytz
-from collections import namedtuple
 from datetime import datetime
 from mock import call
 
@@ -14,7 +13,6 @@ def config():
     return {
         "airflow": False,
         "artifact_path": "s3://us-east-1.elasticmapreduce/samples/wordcount/",
-        "checkpoint_bucket": "",
         "cluster_name": "Sandbox",
         "dryrun": False,
         "emr_version": "emr-5.6.0",
@@ -23,12 +21,10 @@ def config():
         "job_name": "WordCount",
         "job_runtime": "Python",
         "job_timeout": 60,
-        "job_mode": "batch",
-        "main_class": "org.apache.hadoop.tools.WordCount",
+        "main_class": "org.apache.spark.examples.WordCount",
         "poll_cluster": False,
         "profile": "qa",
         "project": "",
-        "shutdown": False,
         "terminate": True
     }
 
@@ -66,18 +62,6 @@ def aws_api(mocker, step_info):
 
 
 @pytest.fixture
-def grequests(mocker):
-    Response = namedtuple('Response', ['status_code'])
-    responses = [Response(200), Response(200)]
-
-    mock_grequests = mocker.patch('emr.job_client.grequests', autospec=True)
-    mock_grequests.post.return_value = None
-    mock_grequests.get.return_value = None
-    mock_grequests.map.return_value = responses
-    return mock_grequests
-
-
-@pytest.fixture
 def time_sleep(mocker):
     return mocker.patch('emr.job_client.time.sleep')
 
@@ -91,20 +75,13 @@ def fixed_datetime(mocker):
 
 
 @pytest.fixture
-def shutdown_function(mocker):
-    shutdown_fn = mocker.patch('emr.job_client.shutdown_streaming_job')
-    shutdown_fn.return_value = False
-    return shutdown_fn
-
-
-@pytest.fixture
 def shell_function(mocker):
     shell_fn = mocker.patch('emr.utils.run_shell_command', autospec=True)
     shell_fn.return_value = "{\"StepIds\": [\"s-F37BY4CL9\"]}"
     return shell_fn
 
 
-def test_adds_expected_python_job(config, aws_api, grequests, shell_function):
+def test_adds_expected_python_job(config, aws_api, shell_function):
     expected_output = \
         """
         aws emr add-steps --profile qa --cluster-id cl-359
@@ -116,6 +93,29 @@ def test_adds_expected_python_job(config, aws_api, grequests, shell_function):
         s3://us-east-1.elasticmapreduce/samples/wordcount/main.py]
         """
 
+    handle_job_request(config)
+
+    shell_function.assert_called_once_with(
+        textwrap.dedent(expected_output).replace('\n', ''))
+
+
+def test_adds_expected_java_job(config, aws_api, shell_function):
+    expected_output = \
+        """
+        aws emr add-steps --profile qa --cluster-id cl-359
+         --steps Type=Spark,Name=WordCount,ActionOnFailure=CONTINUE,
+        Args=[--deploy-mode,cluster,--master,yarn,--conf,
+        'spark.app.name=WordCount',--class,org.apache.spark.examples.WordCount,
+        --conf,'spark.driver.extraJavaOptions=-DenvironmentKey=qa',
+        --conf,'spark.executor.extraJavaOptions=-DenvironmentKey=qa',
+        s3://us-east-1.elasticmapreduce/samples/wordcount.jar,
+        hdfs:///text-input/]
+        """
+
+    config['job_runtime'] = 'Java'
+    config['artifact_path'] = \
+        's3://us-east-1.elasticmapreduce/samples/wordcount.jar'
+    config['job_args'] = 'hdfs:///text-input/'
     handle_job_request(config)
 
     shell_function.assert_called_once_with(
@@ -134,7 +134,6 @@ def test_invalid_job_runtime_throws_error(config, aws_api, shell_function):
 def test_missing_output_resource_id_raises_error(config,
                                                  aws_api,
                                                  step_info,
-                                                 grequests,
                                                  time_sleep,
                                                  fixed_datetime,
                                                  shell_function):
@@ -148,7 +147,6 @@ def test_missing_output_resource_id_raises_error(config,
 def test_polls_until_job_state_completed(config,
                                          step_info,
                                          aws_api,
-                                         grequests,
                                          time_sleep,
                                          fixed_datetime,
                                          shell_function):
@@ -176,7 +174,6 @@ def test_polls_until_job_state_completed(config,
 def test_polls_until_job_state_failed(config,
                                       step_info,
                                       aws_api,
-                                      grequests,
                                       time_sleep,
                                       fixed_datetime,
                                       shell_function):
@@ -204,10 +201,43 @@ def test_polls_until_job_state_failed(config,
     aws_api.return_value.list_cluster_steps.assert_has_calls(expected_calls)
 
 
+def test_does_not_terminate_cluster_when_disabled(config,
+                                                  step_info,
+                                                  aws_api,
+                                                  time_sleep,
+                                                  fixed_datetime,
+                                                  shell_function):
+    step_info[0]['Status']['State'] = 'COMPLETED'
+    aws_api.return_value.list_cluster_steps.return_value = step_info
+
+    # set terminate to false
+    config['poll_cluster'] = True
+    config['terminate'] = False
+    handle_job_request(config)
+
+    assert not aws_api.return_value.terminate_clusters.called
+
+
+def test_terminates_cluster_when_enabled(config,
+                                         step_info,
+                                         aws_api,
+                                         time_sleep,
+                                         fixed_datetime,
+                                         shell_function):
+    step_info[0]['Status']['State'] = 'COMPLETED'
+    aws_api.return_value.list_cluster_steps.return_value = step_info
+
+    # set terminate to true
+    config['poll_cluster'] = True
+    config['terminate'] = True
+    handle_job_request(config)
+
+    assert aws_api.return_value.terminate_clusters.call_count == 1
+
+
 def test_polls_until_job_state_timeout(config,
                                        step_info,
                                        aws_api,
-                                       grequests,
                                        time_sleep,
                                        fixed_datetime,
                                        shell_function):
@@ -236,66 +266,18 @@ def test_polls_until_job_state_timeout(config,
     aws_api.return_value.list_cluster_steps.assert_has_calls(expected_calls)
 
 
-def test_shutdown_false_no_shutdown_api_calls(config,
-                                              aws_api,
-                                              grequests,
-                                              shutdown_function,
-                                              shell_function):
-    config['job_runtime'] = 'Scala'
-    config['shutdown'] = False
-    handle_job_request(config)
-
-    assert not shutdown_function.called
-    assert not aws_api.return_value.add_checkpoint_copy_job.called
-
-
-def test_shutdown_true_makes_shutdown_api_calls(config,
-                                                step_info,
-                                                aws_api,
-                                                grequests,
-                                                time_sleep,
-                                                shutdown_function,
-                                                shell_function):
-    config['job_runtime'] = 'Scala'
-    config['shutdown'] = True
-
-    # mock responses for shutdown polling
-    step_info[0]['Name'] = 'S3DistCp'
-    step_info[0]['Status']['State'] = 'COMPLETED'
-    aws_api.return_value.list_cluster_steps.return_value = step_info
-
-    handle_job_request(config)
-
-    assert shutdown_function.call_count == 1
-    assert aws_api.return_value.add_checkpoint_copy_job.call_count == 1
-
-
 def test_cluster_job_not_added_conditions(config,
                                           step_info,
                                           aws_api,
-                                          grequests,
                                           time_sleep,
-                                          shutdown_function,
                                           shell_function):
-    # job should not be added if artifact_path is empty, or shutdown is True
-    config['shutdown'] = True
-
-    # mock responses for shutdown polling
-    step_info[0]['Name'] = 'S3DistCp'
-    step_info[0]['Status']['State'] = 'COMPLETED'
-    aws_api.return_value.list_cluster_steps.return_value = step_info
-
-    handle_job_request(config)
-    assert not shell_function.called
-
+    # job should not be added if artifact_path is empty
     config['artifact_path'] = ''
-    config['shutdown'] = False
     handle_job_request(config)
     assert not shell_function.called
 
     # job should be added under these parameters
     config['artifact_path'] = \
         's3://us-east-1.elasticmapreduce/samples/wordcount/'
-    config['shutdown'] = False
     handle_job_request(config)
     assert shell_function.call_count == 1
