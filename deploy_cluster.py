3be151a781f1e18668e1f6e1baed9c21ebed0eae
Caleb Sabatini <calebsabatini@gmail.com>
2026-01-09 13:20:24 -0600

    refactory cluster script for airflow; add polling of EMR steps

---
+++ deploy_cluster.py
@@ -0,0 +1,104 @@
+import json
+import click
+from jinja2 import Template
+from awsutils import get_clients, get_emr_cluster_with_name, tokenize_emr_step_args, run_cli_cmd
+from templates import spark_template
+
+profiles = {
+    'qa': 'saml3',
+    'pre-prod': 'saml4',
+    'prod': 'saml5'
+}
+valid_runtimes = ['scala', 'python']
+emr_create_cli_template = Template('''aws emr create-cluster{% if not airflow %} --profile {{ profile }}{% endif %}
+    \t--name {{ cluster_name }}
+    \t--release-label {{ emr_version }}
+    \t--configurations file://configurations.json
+    \t--{{ term_choice }}
+    \t--service-role {{ emr_service_role }}
+    \t--tags env={{ env }} owner=bigdata project={{ project }} Name={{ cluster_name }}
+    \t--applications Name=Hadoop Name=Spark Name=Hue 
+    \t--bootstrap-actions Path=s3://{{ env }}-sonic-artifacts/scripts/{% if job_runtime.lower() == "python" %}pyspark_bootstrap.sh,Name=PysparkBootstrap,Args=[{{ artifact_path }}]{% else %}download_service.sh,Name=S3DownloadService,Args=[{{ artifact_path }}]{% endif %} 
+    \t--ec2-attributes InstanceProfile={{ emr_instance_profile }},KeyName={{ emr_ssh_key_name }},SubnetIds=[\'{{ master_private_subnet_app_secondary_id }}\'],ServiceAccessSecurityGroup={{ master_emrservice_security_group_id }},EmrManagedMasterSecurityGroup={{ master_emr_security_group_id }},EmrManagedSlaveSecurityGroup={{ master_emr_security_group_id }} 
+    \t--instance-groups InstanceGroupType=MASTER,InstanceCount=1,InstanceType={{ master_type }} InstanceGroupType=CORE,InstanceType={{ core_type }},InstanceCount={{ core_count }} 
+    \t{% if artifact_path %}--steps Type=Spark,Name={{ job_name }},ActionOnFailure=CONTINUE,Args={{ step_args }}{% endif %}
+''')
+
+
+@click.command()
+@click.pass_context
+@click.option('--env', prompt='Enter the environment to deploy to (qa/pre-prod/prod)',
+              help='Environment to deploy to (required).')
+@click.option('--emr_version', default='emr-5.6.0', help='Service version for EMR.')
+@click.option('--job_name', help='Name for the EMR Step & Spark job')
+@click.option('--job_runtime', default='scala', help='Runtime for Spark, should be either Scala or Python.')
+@click.option('--cluster_name', default='DataPipeline', help='Name for the EMR cluster.')
+@click.option('--project', default='know_your_customer', help='Project to tag the AWS resources with.')
+@click.option('--main_class', default='com.sonicdrivein.datapipeline.Main', help='Main class of the Spark application.')
+@click.option('--artifact_path', help='Amazon S3 path to the Spark artifact.')
+@click.option('--core_count', default=2, help='Number of core cluster nodes.')
+@click.option('--core_type', default='m3.xlarge', help='EC2 instance type for the core node(s).')
+@click.option('--master_type', default='m1.large', help='EC2 instance type for the master node.')
+@click.option('--h2o_backend', is_flag=True, help='Indicates that the Spark job uses the H2O backend.')
+@click.option('--auto-terminate', 'term_choice', flag_value='auto-terminate',
+              help='Terminate the cluster after the Spark job finishes.')
+@click.option('--no-auto-terminate', 'term_choice', flag_value='no-auto-terminate', default=True,
+              help='Keep the cluster running after the Spark job finishes (default).')
+@click.option('--cicd', is_flag=True, help='Indicator for deployment from gocd; uses IAM profile auth.')
+@click.option('--airflow', is_flag=True, help='Indicator for deployment from airflow; uses EC2 instance role auth.')
+def deploy(ctx, env, emr_version, job_name, job_runtime, cluster_name, project, main_class,
+           artifact_path, core_count, core_type, master_type, h2o_backend, term_choice, cicd, airflow):
+    config = ctx.params
+    assert job_runtime.lower() in valid_runtimes, 'job_runtime must be either Scala or Python'
+    config['step_args'] = None if not artifact_path else tokenize_emr_step_args(spark_template.render(config))
+    config['profile'] = 'bdp-{}'.format(env.replace('-', '')) if cicd else profiles[env]  # gocd profiles are bdp-{env}
+
+    # create the boto clients for this environment
+    s3_client, emr_client = get_clients(config['profile'])
+
+    terminate_clusters(emr_client, cluster_name, config)
+
+    # read terrraform state for the account (IAM)
+    get_s3_state(s3_client, env, config, '{}-master/account.tfstate'.format(env),
+                 ['emr_service_role', 'emr_instance_profile', 'emr_ssh_key_name'])
+    # read terraform state for the network (VPC)
+    get_s3_state(s3_client, env, config, '{}-master/shared.tfstate'.format(env),
+                 ['master_private_subnet_app_secondary_id', 'master_emrservice_security_group_id',
+                  'master_emr_security_group_id'])
+
+    if not cicd and not airflow:
+        print '\n{}\n\nPress enter to confirm the cluster config'.format(json.dumps(config, indent=4, sort_keys=True)),
+        raw_input()
+
+    cli_cmd = emr_create_cli_template.render(config)
+    print '\nRunning script: \n\n{}\n\n'.format(cli_cmd)
+    output = run_cli_cmd(cli_cmd)
+    print output
+    assert 'ClusterId' in json.loads(output).keys(), 'Failed to provision cluster'
+
+
+def terminate_clusters(emrclient, clustername, config):
+    clusters = get_emr_cluster_with_name(emrclient, clustername)
+    terminate_template = Template(
+        'aws emr{% if not airflow %} --profile {{ profile }}{% endif %} terminate-clusters --cluster-id {{ clust_id }}'
+    )
+
+    for cluster_details in clusters:
+        config['clust_id'] = cluster_details['id']
+        print '\n\nTerminating existing cluster: {}'.format(json.dumps(cluster_details))
+        term_command = terminate_template.render(config)
+        print term_command
+        run_cli_cmd(term_command)
+
+
+def get_s3_state(s3client, environment, config, s3_key, output_keys):
+    obj = s3client.get_object(Bucket='{}-sonic-terraform-state-files'.format(environment), Key=s3_key)
+
+    state = json.loads(obj['Body'].read())
+    outputs = state['modules'][0]['outputs']
+    for k in output_keys:
+        config[k] = outputs[k]['value']
+
+
+if __name__ == '__main__':
+    deploy()
--- job_client.py
+++ job_client.py
@@ -2,6 +2,9 @@ import click
 import logging
 import sys
 import json
+import time
+import pytz
+from datetime import datetime, timedelta
 from jinja2 import Template
 from awsutils import get_clients, get_emr_cluster_with_name, tokenize_emr_step_args, run_cli_cmd
 from templates import spark_template
@@ -14,26 +17,56 @@ emr_add_step_template = Template('aws emr --profile {{ env }} add-steps --cluste
 @click.pass_context
 @click.option('--env', prompt='Enter the environment to deploy to (qa/pre-prod/prod)',
               help='Environment to deploy to (required).')
-@click.option('--job_name', default='DataPipeline', help='Name for the Spark job.')
+@click.option('--job_name', help='Name for the EMR Step & Spark job.')
+@click.option('--job_runtime', default='scala', help='Runtime for Spark, should be either Scala or Python.')
+@click.option('--job_timeout', default=120, help='Spark job timeout in minutes.')
 @click.option('--cluster_name', default='DataPipeline', help='Name for the EMR cluster.')
 @click.option('--main_class', default='com.sonicdrivein.datapipeline.Main', help='Main class of the Spark application.')
-@click.option('--artifact_path', default='s3://qa-sonic-enterprise-data-hub/artifacts/pipeline.jar',
-              help='Amazon S3 path to the Spark artifact.')
+@click.option('--artifact_path', help='Amazon S3 path to the Spark artifact.')
 @click.option('--h2o_backend', is_flag=True, help='Indicates that the Spark job uses the H2O backend.')
-def handle_job_request(ctx, env, job_name, cluster_name, main_class, artifact_path, h2o_backend):
+@click.option('--poll-steps', is_flag=True, help='Option to describe the state of the steps on a cluster.')
+def handle_job_request(ctx, env, job_name, job_runtime, job_timeout, cluster_name,
+                       main_class, artifact_path, h2o_backend, poll_steps):
     config = ctx.params
-    if artifact_path:
-        # initialize the aws clients
-        s3_client, emr_client = get_clients(env)
 
-        # get existing cluster info
-        cluster_info = get_emr_cluster_with_name(emr_client, cluster_name)
-        assert len(cluster_info) == 1, \
-            "expected one but found {} cluster(s) with name {}".format(len(cluster_info), cluster_name)
-        config['cluster_id'] = cluster_info[0]['id']
-        cluster_info_json = json.dumps(cluster_info)
-        logging.info("action=get-clusters, result=success, count={}, value={}".format(len(cluster_info),
-                                                                                      cluster_info_json))
+    # initialize the aws clients
+    s3_client, emr_client = get_clients(env)
+
+    # get existing cluster info
+    cluster_info = get_emr_cluster_with_name(emr_client, cluster_name)
+    assert len(cluster_info) == 1, \
+        "expected one but found {} cluster(s) with name {}".format(len(cluster_info), cluster_name)
+
+    # add cluster id to the config and log cluster details
+    config['cluster_id'] = cluster_info[0]['id']
+    cluster_info_json = json.dumps(cluster_info)
+    logging.info("action=get-clusters, count={}, clusterList={}".format(len(cluster_info),
+                                                                        cluster_info_json))
+    if poll_steps:  # monitor state of the EMR Steps (Spark Jobs)
+        minutes_elapsed = 0
+        while minutes_elapsed <= job_timeout:
+            response = emr_client.list_steps(
+                ClusterId=config['cluster_id']
+            )
+            if minutes_elapsed == 0:
+                logging.info("action=list-cluster-steps, clusterId={}, numSteps={}".format(config['cluster_id'],
+                                                                                           len(response['Steps'])))
+            jobs = [s for s in response['Steps'] if s['Name'] == job_name]
+            assert len(jobs) == 1, \
+                "expected one but found {} job(s) with name {}".format(len(job_steps), job_name)
+            job_metrics = cluster_step_metrics(jobs[0])
+            logging.info("action=poll-cluster-step, stepId={}, stepName={}, state={}, createdTime={}, minutesElapsed={}"
+                         .format(job_metrics['id'], job_metrics['name'], job_metrics['state'],
+                                 job_metrics['createdTime'], job_metrics['minutesElapsed']))
+            if job_metrics['state'] == 'COMPLETED':
+                sys.exit(0)  # job successfuly
+            elif job_metrics['state'] in ['CANCELLED', 'FAILED', 'INTERRUPTED']:
+                raise ValueError('Job in unexpected state: {}'.format(job_metrics['state']))
+            minutes_elapsed = job_metrics['minutesElapsed']
+            time.sleep(60)
+        raise ValueError('Job exceeded timeout: {} minutes'.format(job_timeout))
+
+    else:  # TODO add a step to the cluster - this needs to be explored, currently just creating 1 cluster per step
         config['step_args'] = tokenize_emr_step_args(spark_template.render(config))
 
         cli_cmd = emr_add_step_template.render(config)
@@ -41,10 +74,23 @@ def handle_job_request(ctx, env, job_name, cluster_name, main_class, artifact_pa
         # output = run_cli_cmd(cli_cmd)
         # print output
         # assert 'StepId' in json.loads(output).keys(), 'Failed to add step to cluster'
-    else:
-        return None
+
+
+def cluster_step_metrics(step_info):
+    created_dt = step_info['Status']['Timeline']['CreationDateTime']
+    str_created_dt = created_dt.strftime("%Y-%m-%dT%H-%M-%S")
+    seconds_elapsed = (datetime.now(pytz.utc) - created_dt).seconds
+    minutes_elapsed = divmod(seconds_elapsed, 60)[0]
+    return {
+        'id': step_info['Id'],
+        'name': step_info['Name'],
+        'state': step_info['Status']['State'],
+        'createdTime': str_created_dt,
+        'minutesElapsed': minutes_elapsed
+    }
 
 
 if __name__ == '__main__':
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(message)s")
+    logging.getLogger("botocore").setLevel(logging.WARNING)
     handle_job_request()
