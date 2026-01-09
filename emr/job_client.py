5c345a9d89fe54ecb1a6c9d32533ec37a3347313
Caleb Sabatini <calebsabatini@gmail.com>
2026-01-09 13:21:50 -0600

    add spark.yarn.max.executor.failures config

--- emr/job_client.py
+++ emr/job_client.py
@@ -66,6 +66,8 @@ def handle_job_request(ctx, env, job_name, job_runtime, job_timeout, cluster_nam
 
         ec2_instances = aws_api.list_running_cluster_instances(config['cluster_id'])
         private_ips = [i['PrivateIpAddress'] for i in ec2_instances['Instances']]
+        config['num_executors'] = len(ec2_instances['Instances']) - 2 # exclude master node and cluster driver node
+
         log_msg = "environment={}, cluster={}, job={}, action=list-cluster-instances, count={}, ips={}" \
             .format(env, cluster_name, job_name, len(private_ips), json.dumps(private_ips))
         log_assertion(len(private_ips) > 0, log_msg,
